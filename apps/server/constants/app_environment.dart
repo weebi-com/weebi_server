97b7cf2c4207e0ea1cac69ecd2b36638de209cbb
Birante SY <birantesy@gmail.com>
2024-08-28 11:10:06 +0000

    Fix JWT_SECRET_KEY env 🚨

---
+++ apps/server/constants/app_environment.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+class AppEnvironment {
+  static int get port {
+    return int.parse(Platform.environment['PORT'] ?? '8080');
+  }
+
+  static String get mongoDbUri {
+    String? mongoDbUri = Platform.environment['MONGO_DB_URI'];
+    if (mongoDbUri == null) {
+      throw Exception('Missing required environment variable: MONGO_DB_URI');
+    }
+    return mongoDbUri;
+  }
+}
