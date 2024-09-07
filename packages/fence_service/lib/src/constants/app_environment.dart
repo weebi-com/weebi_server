e060c1cfae5c8a681fae2446fcac9b198d0c325f
Flutter Painter <dev@weebi.com>
2024-09-07 12:52:08 +0200

    fix unit test add more checks on chain and boutique creation

--- packages/fence_service/lib/src/constants/app_environment.dart
+++ packages/fence_service/lib/src/constants/app_environment.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 class AppEnvironment {
   static String get jwtSecretKey {
+    if (Platform.environment['SESSIONNAME'] == 'Console') {
+      return 'test';
+    }
     String? jwtSecretKey = Platform.environment['JWT_SECRET_KEY'];
     if (jwtSecretKey == null) {
       throw Exception('Missing required environment variable: JWT_SECRET_KEY');
