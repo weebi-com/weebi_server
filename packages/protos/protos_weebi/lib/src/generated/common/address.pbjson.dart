c886da6159f2c1ec5f360ad7c0e5d4249f69625c
Flutter Painter <dev@weebi.com>
2025-01-22 23:27:08 +0100

    do not use proto3 optional instead use comments to guide users

--- packages/protos/protos_weebi/lib/src/generated/common/address.pbjson.dart
+++ packages/protos/protos_weebi/lib/src/generated/common/address.pbjson.dart
@@ -21,12 +21,8 @@ const Address$json = {
     {'1': 'code', '3': 2, '4': 1, '5': 9, '10': 'code'},
     {'1': 'city', '3': 3, '4': 1, '5': 9, '10': 'city'},
     {'1': 'country', '3': 4, '4': 1, '5': 11, '6': '.weebi.common.country.Country', '10': 'country'},
-    {'1': 'latitude', '3': 5, '4': 1, '5': 1, '9': 0, '10': 'latitude', '17': true},
-    {'1': 'longitude', '3': 6, '4': 1, '5': 1, '9': 1, '10': 'longitude', '17': true},
-  ],
-  '8': [
-    {'1': '_latitude'},
-    {'1': '_longitude'},
+    {'1': 'latitude', '3': 5, '4': 1, '5': 1, '10': 'latitude'},
+    {'1': 'longitude', '3': 6, '4': 1, '5': 1, '10': 'longitude'},
   ],
 };
 
@@ -34,7 +30,6 @@ const Address$json = {
 final $typed_data.Uint8List addressDescriptor = $convert.base64Decode(
     'CgdBZGRyZXNzEhYKBnN0cmVldBgBIAEoCVIGc3RyZWV0EhIKBGNvZGUYAiABKAlSBGNvZGUSEg'
     'oEY2l0eRgDIAEoCVIEY2l0eRI3Cgdjb3VudHJ5GAQgASgLMh0ud2VlYmkuY29tbW9uLmNvdW50'
-    'cnkuQ291bnRyeVIHY291bnRyeRIfCghsYXRpdHVkZRgFIAEoAUgAUghsYXRpdHVkZYgBARIhCg'
-    'lsb25naXR1ZGUYBiABKAFIAVIJbG9uZ2l0dWRliAEBQgsKCV9sYXRpdHVkZUIMCgpfbG9uZ2l0'
-    'dWRl');
+    'cnkuQ291bnRyeVIHY291bnRyeRIaCghsYXRpdHVkZRgFIAEoAVIIbGF0aXR1ZGUSHAoJbG9uZ2'
+    'l0dWRlGAYgASgBUglsb25naXR1ZGU=');
 
